93e9aa80e146d85060cb6a8c7d7e9036cf91af4e
Herbert Vianna <herbertvianna@gmail.com>
2018-11-25 01:56:30 -0200

    Inclusão de Vendedor (view Create)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -15,9 +15,15 @@ namespace SalesWebMVC.Services
             _context = context;
         }
 
-        public List<Seller> FIndAll()
+        public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
